40a9287c895c479c3ec8208006fc3d4aecb7adf8
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-04-07 14:16:41 -0700

    Deleted some comments and added logic to read in county name from user

--- express.js
+++ express.js
@@ -1,61 +1,31 @@
 import { put } from 'axios';
 import createApp from 'ringcentral-chatbot/dist/apps';
 import getData from './api/covid';
-// const url = require('./assets/jackson.png');
-
+let county = 'nothing';
 const handle = async (event) => {
     const covid = await getData();
-    console.log(typeof covid);
-
-    // console.log(`Cases: ${cases} and COunty: ${county}`);
 
     const { type, text, group, bot } = event;
-    // console.log(`TEXT: ${event.text}`);
+
+    if (event.text !== undefined) {
+        county = event.text;
+    }
+    console.log('TEXT FROM EVENT: ' + county);
 
     if (type === 'Message4Bot' && text === 'pong') {
         // for (let i = 0; i < covid.length; i++) {
         await bot.sendMessage(group.id, {
-            // text: 'Body of the post',
-            // attachments: [
-            //     {
-            //         type: 'Card',
-            //         fallback: 'Attachment fallback text',
-            //         color: '#00ff2a',
-            //         intro:
-            //             'Attachment intro appears before the attachment block',
-            //         author: {
-            //             name: 'Author Name',
-            //             uri: 'https://example.com/author_link',
-            //             iconUri: url,
-            //         },
-            //         title: 'Attachment Title',
-            //         text: 'Attachment text',
-            //         imageUri: 'https://example.com/congrats.gif',
-            //         thumbnailUri: 'https://example.com/thumbnail_icon.png',
-
-            //         footnote: {
-            //             text: 'Attachment footer and timestamp',
-            //             iconUri: 'https://example.com/footer_icon.png',
-            //             time: '2018-01-05T18:52:35.993311508-08:00',
-            //         },
-            //     },
-            // ],
             attachments: [
                 {
                     type: 'Card',
-                    text: `Covid Cases for **${
-                        covid[0].county
-                    }**: \n${covid
-                        .map(
-                            (data) =>
-                                `Cases: **${data.cases}** - Date: **${data.date}**`
-                        )
-                        .join('\n\n')}`,
+                    text: `Covid Cases for **${covid[0].county}**`,
+                    // : \n${covid
+                    //     .map(
+                    //         (data) =>
+                    //             `Cases: **${data.cases}** - Date: **${data.date}**`
+                    //     )
+                    //     .join('\n\n')}`,
                     author_name: 'Author Name',
-                    author_link: 'https://example.com/author_link',
-                    author_icon: 'https://example.com/author_icon.png',
-                    // author_name: 'jackson1',
-                    // author_icon: '../assets/jackson.png',
 
                     footnote: {
                         text:
