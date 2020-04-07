313bca67a620966fa85e44c67f35d7b41fbf2537
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-04-07 13:26:06 -0700

    Accidently pushed all these files under oe commit and I dont know how to change it. So I'm just going to list here what I did:
    	Added csvtojson.
    	Added API call to NY times
    	Bot now prints all the case info for Washoe county
    	Currently using a card format

--- api/covid.js
+++ api/covid.js
@@ -1,16 +1,46 @@
-const getData = async () => {
-    const covid = [
-        {
-            county: 'Washoe',
-            cases: 10,
-        },
-        {
-            county: 'Nye',
-            cases: 12,
-        },
-    ];
+const axios = require('axios');
+const CSVToJSON = require('csvtojson');
+const fsLibrary = require('fs');
 
-    return covid;
+const getData = async () => {
+    let response;
+    try {
+        response = await axios.get(
+            'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+        );
+
+        fsLibrary.writeFile('cases.csv', response.data, (error) => {
+            // In case of a error throw err exception.
+            if (error) throw err;
+        });
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    return response;
 };
 
-module.exports = getData;
+const toJSON = async () => {
+    let formattedData;
+    let countyData = [];
+    await getData();
+    try {
+        formattedData = await CSVToJSON()
+            .fromFile('./cases.csv')
+            .then((source) => {
+                for (let i = 0; i < source.length; i++) {
+                    if (source[i].county === 'Washoe') {
+                        countyData.push(source[i]);
+                    }
+                }
+                return countyData;
+            });
+    } catch (error) {
+        console.log(error);
+    }
+
+    return formattedData;
+};
+
+module.exports = toJSON;
--- express.js
+++ express.js
@@ -1,17 +1,68 @@
 const axios = require('axios');
 const createApp = require('ringcentral-chatbot/dist/apps').default;
 const getData = require('./api/covid');
+// const url = require('./assets/jackson.png');
 
 const handle = async (event) => {
     const covid = await getData();
-    console.log(covid);
-    const { cases, county } = covid[1];
-    console.log(`Cases: ${cases} and COunty: ${county}`);
+    console.log(typeof covid);
+
+    // console.log(`Cases: ${cases} and COunty: ${county}`);
+
     const { type, text, group, bot } = event;
+    // console.log(`TEXT: ${event.text}`);
+
     if (type === 'Message4Bot' && text === 'ping') {
+        // for (let i = 0; i < covid.length; i++) {
         await bot.sendMessage(group.id, {
-            text: `Cases: ${cases} and County: ${county}`,
-            // text: 'ping',
+            // text: 'Body of the post',
+            // attachments: [
+            //     {
+            //         type: 'Card',
+            //         fallback: 'Attachment fallback text',
+            //         color: '#00ff2a',
+            //         intro:
+            //             'Attachment intro appears before the attachment block',
+            //         author: {
+            //             name: 'Author Name',
+            //             uri: 'https://example.com/author_link',
+            //             iconUri: url,
+            //         },
+            //         title: 'Attachment Title',
+            //         text: 'Attachment text',
+            //         imageUri: 'https://example.com/congrats.gif',
+            //         thumbnailUri: 'https://example.com/thumbnail_icon.png',
+
+            //         footnote: {
+            //             text: 'Attachment footer and timestamp',
+            //             iconUri: 'https://example.com/footer_icon.png',
+            //             time: '2018-01-05T18:52:35.993311508-08:00',
+            //         },
+            //     },
+            // ],
+            attachments: [
+                {
+                    type: 'Card',
+                    text: `Covid Cases for **${
+                        covid[0].county
+                    }**: \n${covid
+                        .map(
+                            (data) =>
+                                `Cases: **${data.cases}** - Date: **${data.date}**`
+                        )
+                        .join('\n\n')}`,
+                    author_name: 'Author Name',
+                    author_link: 'https://example.com/author_link',
+                    author_icon: 'https://example.com/author_icon.png',
+                    // author_name: 'jackson1',
+                    // author_icon: '../assets/jackson.png',
+
+                    footnote: {
+                        text:
+                            'This bot was made by Jackson Melcher, the code can be found [here](https://github.com/jacksonmelcher/COVID19-bot)',
+                    },
+                },
+            ],
         });
     }
 };
