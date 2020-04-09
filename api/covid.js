b7513384c1aaae65da9db0ac3cfeabec98b3b0ea
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-04-09 11:40:24 -0700

    Added logic so error about undefined goes away

--- api/covid.js
+++ api/covid.js
@@ -1,58 +1,54 @@
-import { get } from 'axios';
-import CSVToJSON from 'csvtojson';
-import { writeFile } from 'fs';
+import { get } from "axios";
+import CSVToJSON from "csvtojson";
+import { writeFile } from "fs";
 
 const getData = async () => {
-    let response;
-    try {
-        response = await get(
-            'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-        );
+  let response;
+  try {
+    response = await get(
+      "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+    );
 
-        writeFile('cases.csv', response.data, (error) => {
-            // In case of a error throw err exception.
-            if (error) throw err;
-        });
-    } catch (error) {
-        console.log(error);
-        return;
-    }
+    writeFile("cases.csv", response.data, (error) => {
+      // In case of a error throw err exception.
+      if (error) throw err;
+    });
+  } catch (error) {
+    console.log(error);
+    return;
+  }
 
-    return response;
+  return response;
 };
 
 const toJSON = async (county) => {
-    let formattedData;
-    let countyData = [];
-    let paramBool = [];
+  let formattedData;
+  let countyData = [];
+  let paramBool = [];
+  await getData();
 
-    if (typeof county !== 'undefined') {
-        paramBool.push(county);
-        console.log('IN JSON function:' + paramBool[0]);
-    } else {
-        // setTimeout(1000);
-    }
-
-    // await getData();
+  if (typeof county !== "undefined") {
+    paramBool.push(county);
+    console.log("IN JSON function:" + paramBool[0]);
     try {
-        formattedData = await CSVToJSON()
-            .fromFile('./cases.csv')
-            .then((source) => {
-                for (let i = 0; i < source.length; i++) {
-                    if (
-                        source[i].county.toUpperCase() ===
-                        paramBool[0].toUpperCase()
-                    ) {
-                        countyData.push(source[i]);
-                    }
-                }
-                return countyData;
-            });
+      formattedData = await CSVToJSON()
+        .fromFile("./cases.csv")
+        .then((source) => {
+          for (let i = 0; i < source.length; i++) {
+            if (source[i].county.toUpperCase() === paramBool[0].toUpperCase()) {
+              countyData.push(source[i]);
+            }
+          }
+          return countyData;
+        });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
+  } else {
+    // setTimeout(1000);
+  }
 
-    return formattedData;
+  return formattedData;
 };
 
 export default toJSON;
