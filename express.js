2b465d07df73717bc54ab0b37fec9bcb533b281a
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-04-10 14:12:00 -0700

    Trying to figure out an intuitive way to seperate state and county names

--- express.js
+++ express.js
@@ -9,25 +9,32 @@ const handle = async (event) => {
   const { type, text, group, bot } = event;
 
   if (typeof text !== "undefined") {
-    let split = text.split(" ");
-    if (split.length == 2) {
+    let split = text.split("-");
+    if (split.length == 3) {
       county.push(split[0]);
       county.push(split[1]);
-      console.log("County at index 1: " + county[0]);
-      console.log("County at index 2: " + county[1]);
+      county.push(split[2]);
+      console.log("Command: " + county[0]);
+      console.log("County: " + county[1]);
+      console.log("State: " + county[2]);
     }
   }
 
-  covid = await getData(county[1]);
+  covid = await getData(county[1], county[2]);
 
   if (type === "Message4Bot" && county[0] === "stats") {
     await bot.sendMessage(group.id, {
       attachments: [
         {
           type: "Card",
-          text: `Covid Cases for **${covid[0].county}**: \n ${covid
-            .map((data) => `Cases: **${data.cases}** - Date: **${data.date}**`)
-            .join("\n\n")}`,
+          text: `Covid Cases for **${covid[0].county} County**  in **${
+            covid[0].state
+          }**: \n\n ${covid
+            .map(
+              (data) =>
+                `Cases:\t \t \t \t **${data.cases}** - Date:\t **${data.date}**`
+            )
+            .join("\n")}`,
           footnote: {
             text:
               "This bot was made by Jackson Melcher using data from the New York times, the code can be found [here](https://github.com/jacksonmelcher/COVID19-bot)",
